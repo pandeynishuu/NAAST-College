b56474001f7a70ada2a10fc1837d29fd1746706c
Sajal Shrestha <codeitsajal@gmail.com>
2023-03-02 12:04:56 +0545

    minor chnage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,14 +61,19 @@
 <!-- Academic Member -->
 <section>
   <div class="container">
-    <div>
-      <VueSlickCarousel :arrows="true" :dots="true">
+   
+      <carousel :autoplay="true" :margin=10 :dots=true :nav="false" :responsive="{0:{items:1,nav:false,dots:true},600:{items:6,nav:false, dots:true}}">
         <div><img src="https://cdn.pixabay.com/photo/2015/12/15/06/42/kids-1093758__340.jpg" alt=""></div>
         <div><img src="https://cdn.pixabay.com/photo/2018/01/17/07/06/laptop-3087585__340.jpg" alt=""></div>
         <div><img src="https://cdn.pixabay.com/photo/2015/11/15/07/47/geometry-1044090__340.jpg" alt=""></div>
         <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
-      </VueSlickCarousel>
-    </div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+        <div><img src="https://cdn.pixabay.com/photo/2016/11/14/03/43/buddhism-1822518__340.jpg" alt=""></div>
+      </carousel>
   </div>
 </section>
 
@@ -90,16 +95,13 @@
 import TemplateView from '@/views/TemplateView'
 import CarouselComponent from '@/components/CarouselComponent'
 import { mapGetters } from 'vuex';
-import VueSlickCarousel from 'vue-slick-carousel'
-  import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-  // optional style for arrows & dots
-  import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import carousel from 'vue-owl-carousel'
 export default {
   name: 'HomeView',
   components: {
     TemplateView,
     CarouselComponent,
-    VueSlickCarousel
+    carousel
   },
   computed:{
     ...mapGetters({
