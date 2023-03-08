ee244467e9968db9bd80844d63e9a9b9dd95ed55
Sajal Shrestha <codeitsajal@gmail.com>
2023-03-08 12:24:07 +0545

    pop up dialog added

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import router from './router'
 import VueFacebookPage from 'vue-facebook-page'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import VModal from 'vue-js-modal'
+
 
  
 // The plugin is loaded here.
 // You have to pass your FB app Id as a second parameter
  
 Vue.use(VueFacebookPage, /** Add your facebook appId here */)
- 
+Vue.use(VModal,{ dialog: true })
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
