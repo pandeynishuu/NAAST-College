84a53c1eb575f42b7e141e62e50b167ef61f584f
Sajal Shrestha <codeitsajal@gmail.com>
2023-03-13 10:32:18 +0545

    minor change

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosApi = axios.create({
-    baseURL: 'https://holygardenacademy.com/api/',  
+    baseURL: 'http://192.168.0.102:8000/api/',  
     headers:{'Accept': 'application/json','Content-Type':'application/json'}
 })
